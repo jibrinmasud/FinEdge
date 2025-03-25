e5f1c326ca385f915e47d82036a1ab929e30f5c4
jibrinmasud <jibrinmasud494@gmail.com>
2025-03-25 17:34:01 +0100

    Generate token for user

--- FinEdge.Infrasturcture/DependencyInjection/ServiceContainer.cs
+++ FinEdge.Infrasturcture/DependencyInjection/ServiceContainer.cs
@@ -1,7 +1,11 @@
+using FinEdge.Application.Interface;
 using FinEdge.Infrasturcture.Data;
+using FinEdge.Infrasturcture.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FinEdge.Infrasturcture.DependencyInjection
 {
@@ -10,6 +14,22 @@ namespace FinEdge.Infrasturcture.DependencyInjection
         public static IServiceCollection InfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<AppDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly(typeof(ServiceContainer).Assembly.FullName)), ServiceLifetime.Scoped);
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = configuration["Jwt:Issuer"],
+                ValidAudience = configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!))
+            });
+
+            services.AddScoped<IUser, UserRepo>();
 
             return services;
         }
--- FinEdge.Infrasturcture/Repository/UserRepo.cs
+++ FinEdge.Infrasturcture/Repository/UserRepo.cs
@@ -1,18 +1,25 @@
-﻿using FinEdge.Application.DTOs;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using FinEdge.Application.DTOs;
 using FinEdge.Application.Interface;
 using FinEdge.Domain.Entities;
 using FinEdge.Infrasturcture.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FinEdge.Infrasturcture.Repository
 {
     public class UserRepo : IUser
     {
         private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
 
-        public UserRepo(AppDbContext context)
+        public UserRepo(AppDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         public async Task<LoginResponse> LoginUserAsync(LoginDto loginDto)
@@ -29,7 +36,23 @@ namespace FinEdge.Infrasturcture.Repository
                 return new LoginResponse(false, "Invalid Login Details");
         }
 
-        //private async Task<AppUser> FindUserByEmail(string emial) => await _context.Users.FirstOrDefaultAsync(u =>u.Email == email)
+        private string GenerateToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName!),
+                new Claim(ClaimTypes.Email, user.Email!),
+            };
+            var token = new JwtSecurityToken(issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Issuer"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
         public async Task<RegisterationResponse> RegisterUserAsync(RegisterUserDto registerUserDto)
         {
             var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == registerUserDto.Email);
